e48dffdf229e147c066eb1fa54dc04a35b921fdc
GzINOD <gzinod31@rambler.ru>
2022-11-17 20:45:42 +0300

    Homework Done

---
+++ HomeTask15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Input number");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1>0 && num1<=7)
+{
+    if(num1==6 || num1==7)
+    {
+        Console.WriteLine("Its weekend!");
+    }
+    else
+    {
+        Console.WriteLine("Nice day for your Job");
+    }
+}
+else
+{
+    Console.WriteLine("incorrect number");
+}
